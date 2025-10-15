446be997fa604c467f521c15c825fbd6afed739e
kensledev <kensledev@gmail.com>
2025-10-15 18:43:37 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -1,10 +1,36 @@
-HOME_DIR=/home/kd
-BIN_DIR=/usr/local/bin
+#HOME_DIR=/home/kd
+#BIN_DIR=/usr/local/bin
 
-# curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+setup() {
+  curl https://mise.run | sh
+  sudo apt install stow -y 
+}
 
-mice install
+ensure_dotfiles_repo() {
+  local repo_dir="$HOME/.dotfiles"
+  local repo_url="https://github.com/kensledev/dotfiles.git"
 
-stow ~/dotfiles/nvim
-stow ~/dotfiles/nvim
-...
+  if [ -d "$repo_dir/.git" ]; then
+    echo "✅ Dotfiles repo already exists at $repo_dir"
+  else
+    echo "⬇️  Cloning dotfiles repo into $repo_dir..."
+    git clone "$repo_url" "$repo_dir" && echo "✅ Clone successful!" || echo "❌ Clone failed."
+  fi
+}
+
+install_apps() {
+  mise install
+}
+
+apply_config() {
+  dirs=($(eza ~/.dotfiles -D --git-ignore))
+  for dir in "${dirs[@]}"; do
+    echo "Processing: $dir"
+    stow -R -t "$HOME" "$dir"
+  done
+}
+
+setup
+install_apps
+ensure_dotfiles_repo
+apply_config
